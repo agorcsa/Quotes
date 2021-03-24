7fc80339021165cafc6524b776bf4382b878ed11
andreeagorcsa <andreea.gorcsa@t-systems.com>
2021-03-24 18:19:25 +0100

    Update: BaseFragment added

---
+++ app/src/main/java/com/example/quotes/BaseFragment.kt
@@ -0,0 +1,10 @@
+package com.example.quotes
+
+import androidx.fragment.app.Fragment
+
+class BaseFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
